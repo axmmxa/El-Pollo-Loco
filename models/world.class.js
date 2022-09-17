fa2e3adaf4114317a3dde98135e64bcb2cd4c73c
Alexander Much <alexandermuch@MacBook-Pro-von-Alexander-14.local>
2022-09-17 13:49:51 +0200

    world/background borders fixes

--- models/world.class.js
+++ models/world.class.js
@@ -1,43 +1,7 @@
 class World {
 
     character = new Character()
-
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ]
-
-    clouds =[
-        new Cloud()
-    ]
-
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png',-719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
-
-        new BackgroundObject('img/5_background/layers/air.png',0 ),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-
-        new BackgroundObject('img/5_background/layers/air.png',719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
-
-        new BackgroundObject('img/5_background/layers/air.png',719*2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 7198*2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*2),
-
-        new BackgroundObject('img/5_background/layers/air.png',719*3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3)
-    ]
+    level = level1
     canvas
     ctx
     keyboard
@@ -61,10 +25,10 @@ class World {
 
         this.ctx.translate(this.camera_x, 0)
 
-        this.addObjectsToMap(this.backgroundObjects)
+        this.addObjectsToMap(this.level.backgroundObjects)
         this.addToMap(this.character)
-        this.addObjectsToMap(this.enemies)
-        this.addObjectsToMap(this.clouds)
+        this.addObjectsToMap(this.level.enemies)
+        this.addObjectsToMap(this.level.clouds)
 
         this.ctx.translate(-this.camera_x, 0)
       
