8e85c1222aa53984568db18c904aee3536b592da
Alexander Much <alexandermuch@MacBook-Pro-von-Alexander-14.local>
2022-09-17 12:40:31 +0200

    world, background extended 4 times

--- models/world.class.js
+++ models/world.class.js
@@ -13,15 +13,35 @@ class World {
     ]
 
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png',0 ,0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 80),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0, 80),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 80)
+        new BackgroundObject('img/5_background/layers/air.png',-719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+
+        new BackgroundObject('img/5_background/layers/air.png',0 ),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+
+        new BackgroundObject('img/5_background/layers/air.png',719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('img/5_background/layers/air.png',719*2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 7198*2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*2),
+
+        new BackgroundObject('img/5_background/layers/air.png',719*3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3)
     ]
     canvas
     ctx
-
     keyboard
+    camera_x = 0
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d')
@@ -33,17 +53,20 @@ class World {
 
     setWorld() {
         this.character.world = this
+
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+        this.ctx.translate(this.camera_x, 0)
+
         this.addObjectsToMap(this.backgroundObjects)
+        this.addToMap(this.character)
+        this.addObjectsToMap(this.enemies)
+        this.addObjectsToMap(this.clouds)
 
-       this.addToMap(this.character)
-
-       this.addObjectsToMap(this.enemies)
-       this.addObjectsToMap(this.clouds)
+        this.ctx.translate(-this.camera_x, 0)
       
         // Draw wird immer wieder aufgerufen
         let self = this
